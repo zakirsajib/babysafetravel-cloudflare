fb481250026c243a5d90f5c48bae437d8ff753e4
Zajur Sajib <zakirsajib@yahoo.co.uk>
2023-07-05 14:03:38 +0600

    added xml sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
+//import netlify from '@astrojs/netlify/functions';
 
 //import node from "@astrojs/node";
 
-// https://astro.build/config
 import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [sitemap()]
 });
