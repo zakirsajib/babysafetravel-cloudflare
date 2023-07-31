fa12826aacd7d1e79c8f3f5283a04a40ff6d964d
Zajur Sajib <zakirsajib@yahoo.co.uk>
2023-07-31 10:42:47 +0600

    Cypress test functionality is added
    We tested title, h1, robots.txt

---
+++ cypress/e2e/index.cy.js
@@ -0,0 +1,20 @@
+it('titles are correct', () => {
+  const page = cy.visit('https://babysafetravel-cloudflare.pages.dev/');
+
+  page.get('title').should('have.text', 'Home Page | Baby Safe Travel')
+  page.get('h1').should('have.text', 'Baby Safe Travel');
+});
+
+
+it('robots.txt exists', () => {
+  const robotsTxtUrl = `${window.location.origin}/robots.txt`;
+
+  cy.request(robotsTxtUrl).then((response) => {
+    // Check if the response status code is 200 (OK)
+    expect(response.status).to.eq(200);
+
+    // You can also add more assertions to check the content, if needed
+    // For example:
+    // expect(response.body).to.contain('User-agent: *');
+  });
+});
